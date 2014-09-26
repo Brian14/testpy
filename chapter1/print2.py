598540eaaf84123a32773a4eba7f0b7f0c47aa4c
Brian14 <brianbao@126.com>
2014-09-27 01:05:06 +0800

    123

---
+++ chapter1/print2.py
@@ -0,0 +1,18 @@
+__author__ = 'bao'
+
+# 转义\"
+print("I am 6'2\" tall")
+
+
+# 转义\'
+print('I am 6\'2" tall')
+
+#三引号和\n换行
+fat_cat = """
+I will do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print(fat_cat)
---
+++ chapter2/input1.py
@@ -0,0 +1,13 @@
+__author__ = 'bao'
+
+print("age")
+age = input()
+
+print("height")
+height = input()
+
+print("input age is: " + age)
+print("input height is: " + height)
+
+#
+# input("age?")
