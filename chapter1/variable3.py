e6ea3aeec0fb3883c2df49f0102201724311a468
Brian Bao <brian.bao@oocl.com>
2014-09-26 17:41:14 +0800

    123

--- chapter1/variable3.py
+++ chapter1/variable3.py
@@ -1,1 +1,28 @@
 __author__ = 'BAOBR'
+
+cars = 100.0
+drivers = 30
+# test
+# print("this is  ", cars + drivers + test, " :end")
+
+# 浮点数的问题
+print(0.1 + 0.2 + 0.1 + 0.2)
+
+name = 'Zed A. Shaw'
+age = 35  # not a lie
+height = 74  # inches
+weight = 180  # lbs
+eyes = 'blue'
+teeth = 'white'
+hair = 'brown'
+
+print("let's talk about %s." % name)
+print("he's %d inches tall" % height)
+print("he's %d pounds heavy" % weight)
+
+# %后面的参数个数需要和前面的格式化字段里的一致
+print("he's %d pounds heavy, and eys is %s" % (weight, eyes))
+
+#类型不符，不能强转
+#print("if i add %d, %d, and %d, i get %d" % (age, height, weight, eyes + height + weight))
+
---
+++ chapter1/variable5.py
@@ -0,0 +1,20 @@
+__author__ = 'BAOBR'
+
+x = "there are %d types of people." % 10
+
+binary = "binary"
+
+do_not = "don't"
+
+y = "those who know %s those who %s" % (binary, do_not)
+
+print(x)
+
+print(y)
+
+# %s和%r的不同
+print("i said: %r" % x)
+print("i said: %s" % x)
+
+
+
