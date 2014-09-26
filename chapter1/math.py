eb4e41764770eb74fd0110c3a632616514a83151
Brian Bao <brian.bao@oocl.com>
2014-09-26 14:50:01 +0800

    123

---
+++ chapter1/math.py
@@ -0,0 +1,10 @@
+__author__ = 'BAOBR'
+
+print("test1: ", 25 + 30 / 6)
+
+print("test2: ", 100 - 25 * 3 % 4)
+
+# boolean
+print("test3: ", 5.0 <= -2)
+
+print("test4: ", 4 >= 3)
---
+++ chapter1/print1.py
@@ -0,0 +1,18 @@
+__author__ = 'BAOBR'
+
+#双引号
+print("hello world")
+
+#单引号
+print('hello again')
+
+#混合
+print('hello "xx" again')
+
+#混合
+print('hello "s again')
+
+#混合且不匹配，语法错
+# print('hello" again '  ss)
+
+
