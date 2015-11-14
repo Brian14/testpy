bdfec50d6b9ffe8bcc99ba0ce78c3eee29514577
Brian14 <brianbao@126.com>
2015-11-14 15:33:54 +0800

    123
    123

---
+++ learnpython2/test list/__init__.py
@@ -0,0 +1,20 @@
+__author__ = 'bao'
+
+
+def showlist():
+    return [3, 4, 5, 5, 6, 7]
+
+
+sample = [1, 2, 3, 4, 5, 8]
+a = sample[1]
+
+b = showlist()[3]
+
+print a
+print b
+print [2, 4, 5, 6, 7, 8][5]
+
+list1 = [1, 2, 3, 4]
+list2 = [2, 3, 4, 5]
+print list1 + list2
+
